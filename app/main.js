b8d25ba8358cbe79b2d368a1ac2555c721b8dd17
ArtemHard <95387501+ArtemHard@users.noreply.github.com>
2022-12-16 13:22:28 +0300

    add solution

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 console.log("hi guys");
 /* Complete the solution so that it returns true if the first argument(string)
  passed in ends with the 2nd argument (also a string).*/
+/*
 const a = "abc";
 const b = "bc";
 const c = "d";
@@ -19,4 +20,28 @@ function solution(str, ending) {
   const check = dublicateArr.join("");
   const example = newArrEnd.join("");
   return check === example;
+}
+*/
+/*
+In this example you have to validate if a user input string is alphanumeric. 
+The given string is not nil/null/NULL/None, so you don't have to check that.
+
+The string has the following conditions to be alphanumeric:
+
+At least one character ("" is not valid)
+Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+No whitespaces / underscore
+*/
+
+const test1 = "Mazinkaiser";
+const test2 = "helloworld_";
+const test3 = "PassW0rd";
+const test4 = "     ";
+const test5 = "";
+
+function alphanumeric(string) {
+  // return string.replace(" ", "");
+  const reg = /[\W_]/gi;
+  return string.length ? !reg.test(string) : false;
+  //your code here
 }
